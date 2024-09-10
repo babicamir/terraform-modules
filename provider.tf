4e3bdf1bef818cd4841d8e32672e90c189d660ac
Amir Babic <Amir.Babic@basistechnologies.com>
2024-09-10 12:46:05 +0200

    updates

---
+++ provider.tf
@@ -0,0 +1,50 @@
+##########################################
+#AWS
+##########################################
+provider "aws" {
+  default_tags {
+    tags = {
+      Project     = var.project-name
+      ManageBy    = "With terraform"
+      Environment = var.env
+    }
+  }
+}
+
+##########################################
+# Other providers
+##########################################
+terraform {
+  # Terraform CLI version
+  #required_version = "1.9.1"
+
+#    backend "s3" {
+#       bucket = "platform-orchestration-ss-terraform-data"
+#       region = "us-east-1"
+#       key = "terraform.tfstate"
+#       workspace_key_prefix = "esgc-solutions-platform"  #Team/project name
+#    }
+
+  required_providers {
+    # AWS
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.56.1"
+    }
+    # tls ssh keys
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.5"
+    }
+    # Random Provider
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.2"
+    }
+    # Local Provider
+    local = {
+      source  = "hashicorp/local"
+      version = "2.5.1"
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+##########################################################
+# Default Variables
+##########################################################
+variable "project-name" {
+  description = "Project (client) name"
+  type        = string
+}
+variable "env" {
+  description = "Current environment"
+  type        = string
+}
+variable "name" {
+  description = "Resource name"
+  type        = string
+}
