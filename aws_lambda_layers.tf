9561d7709b410ff4d8013ce3d7d231da431cd72f
Amir Babic <Amir.Babic@basistechnologies.com>
2024-09-12 15:40:16 +0200

    Update aws_lambda_layers.tf

--- aws_lambda_layers.tf
+++ aws_lambda_layers.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_layer_version" "lambda" {
   count = var.enable_lambda_layer ? 1 : 0
   layer_name          = "${var.project-name}-${var.env}-${var.name}"
   description         = "Description test"
-  compatible_runtimes = var.architectures
+  compatible_runtimes = "${var.runtime}"
   s3_bucket           = "lambda-layers"
   s3_key              = "assemblyai_layer/assemblyai_layer.zip"
 }
