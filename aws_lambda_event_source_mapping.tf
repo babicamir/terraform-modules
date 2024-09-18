3b7b103dbc435a23c36e8f81a5a8a33386dc018d
Amir Babic <Amir.Babic@basistechnologies.com>
2024-09-18 13:51:37 +0200

    Update aws_lambda_event_source_mapping.tf

--- aws_lambda_event_source_mapping.tf
+++ aws_lambda_event_source_mapping.tf
@@ -35,7 +35,7 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     #id = "lambda-${var.project-name}-${var.env}-${var.name}"
     lambda_function_arn = "arn:aws:lambda:us-east-1:647692764445:function:solution-1-dev-test4"
     events              = var.aws_s3_bucket_notification_events
-    filter_prefix       = var.aws_s3_bucket_notification_filter_prefix
+    filter_prefix       = "Folder4/"
     filter_suffix       = var.aws_s3_bucket_notification_filter_suffix 
   }
 
