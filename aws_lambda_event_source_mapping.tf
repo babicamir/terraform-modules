1d31f6016027c98fe6f5fbc26467038cdfa19624
Amir Babic <Amir.Babic@basistechnologies.com>
2024-09-18 15:39:14 +0200

    Update aws_lambda_event_source_mapping.tf

--- aws_lambda_event_source_mapping.tf
+++ aws_lambda_event_source_mapping.tf
@@ -2,6 +2,7 @@ resource "aws_lambda_event_source_mapping" "dynamodb" {
   count = var.aws_s3_bucket_notification_enabled ? 1 : 0
   event_source_arn = data.aws_dynamodb_table.dynamodb[0].stream_arn
   function_name    = aws_lambda_function.lambda.arn
+  starting_position = "LATEST"
 }
 
 
